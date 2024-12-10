2c50c082e2882a61eaf939d11283bfc5e9957233
shincc6500 <130081283+shincc6500@users.noreply.github.com>
2024-12-10 09:10:29 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    list = [['one','1'],['two','2'],['three','3'],['four','4'],['five','5'],['six','6'],['seven','7'],['eight','8'],['nine','9'],['zero','0']]
+    for i in list:
+        a,b= i
+        numbers = numbers.replace(a,b)
+    
+    
+    return int(numbers)
